c4e8c675a57f13a6f4e783ecbf2a6596d779c0bb
tantan-chan <tantan-chan@users.noreply.github.com>
2021-01-14 18:02:47 +0000

    deploy: 7b304f150561295dea22a74aa851aaafd19144a7

---
+++ precache-manifest.087a79e5efd34a8c2fd6586328a32cf8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2944a163253c0462dd",
+    "url": "/vue-sw/css/app.b569265f.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-sw/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2201fab3d6a9ea09b4cb5ab9ac510044",
+    "url": "/vue-sw/index.html"
+  },
+  {
+    "revision": "d8610e7df79d80c0c835",
+    "url": "/vue-sw/js/about.90b284f2.js"
+  },
+  {
+    "revision": "4e2944a163253c0462dd",
+    "url": "/vue-sw/js/app.cd614d7b.js"
+  },
+  {
+    "revision": "b94f1a238c019a56bfae",
+    "url": "/vue-sw/js/chunk-vendors.148a9483.js"
+  },
+  {
+    "revision": "754450168aa8ece0bc592243270c61eb",
+    "url": "/vue-sw/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-sw/robots.txt"
+  }
+]);
